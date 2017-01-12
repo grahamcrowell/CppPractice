4ce76fc7506fe3109fbc5b7e662da5b253c79257
Crowell <GCrowell@vch.ca>
2017-01-11 20:01:31 -0800

    add git commands

--- git commit.bat
+++ git commit.bat
@@ -1,3 +1,14 @@
 git add .
 git commit -m"commit comment"
 git push -u origin master
+
+echo "# SqlServerUtilities" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/grahamcrowell/SqlServerUtilities.git
+git push -u origin master
+
+git pull origin master
+
+
