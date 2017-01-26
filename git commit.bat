9f397bf78a395faad00e5340c8588b1e8295c30e
grahamcrowell <graham.crowell@gmail.com>
2017-01-25 21:57:02 -0800

    commit comment

--- git commit.bat
+++ git commit.bat
@@ -2,10 +2,12 @@ git add .
 git commit -m"commit comment"
 git push -u origin master
 
-echo "# SqlServerUtilities" >> README.md
+echo "# VS-Code-CPP-TEMPLATE" >> README.md
 git init
 git add README.md
 git commit -m "first commit"
+explorer "https://github.com/new"
+pause
 git remote add origin https://github.com/grahamcrowell/SqlServerUtilities.git
 git push -u origin master
 
