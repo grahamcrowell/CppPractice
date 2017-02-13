5b6b0cb11cb9c4d968ff1bfbb4cb14b6d0e031aa
grahamcrowell_laptop <graham.crowell@gmail.com>
2017-02-13 08:36:45 -0800

    added clean step to buid.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,19 @@
 @echo off
+
+REM delete old files (force a full recompile each build)
+echo CLEANING...
+DEL /F main.exe
+DEL /F main.ilk
+DEL /F main.obj
+DEL /F main.pbd
+
+echo BUILDING...
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64     
 set compilerflags=/Od /Zi /EHsc
 set linkerflags=/OUT:main.exe
 cl.exe %compilerflags% main.cpp /link %linkerflags%
+
+echo RUNNING...
+start main.exe
+
+echo EXECUTION COMPLETE...
